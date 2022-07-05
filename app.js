c4c07acbe25596120fae70640fe37beae7ffa15b
Arjun Mahanty <arjunmahanty2002@gmail.com>
2022-07-05 19:49:46 +0530

    Fourth commit

--- app.js
+++ app.js
@@ -46,16 +46,8 @@ app.post("/compose",function(req,res){
   res.redirect("/");
 })
 
+const PORT=process.env.PORT||3000
 
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
